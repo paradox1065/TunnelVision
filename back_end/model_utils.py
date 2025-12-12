aba03e946e1d85f4d3a55a517edd85399f6755ec
paradox1065 <kavyat1102@gmail.com>
2025-12-12 20:55:38 +0000

    Refactor predict_features function to improve feature documentation

--- back_end/model_utils.py
+++ back_end/model_utils.py
@@ -2,17 +2,7 @@ import joblib
 
 rf = joblib.load("model.pkl")
 def predict_features(features: list):
-    '''features = [
-        type, 
-        material, 
-        region, 
-        soil_type, 
-        exact_location, 
-        temperature,_c, 
-        date_of_last_repair, 
-        date_issue_was_observed, 
-        install_year, 
-        issue_description
-        ]''' # reminder for me, NOT CODE
+    # reminder for me, NOT CODE ↓
+    # features = [type, material, region, soil_type, exact_location, temperature_c, date_of_last_repair, date_issue_was_observed, install_year, issue_description]
     
     return str(rf.predict([features])[0])
