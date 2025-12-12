ca8e5357fc3c9e810015926e2b106beacedb03ea
paradox1065 <kavyat1102@gmail.com>
2025-12-12 20:56:15 +0000

    Fixed formatting for cleaner appearance and consistency.

--- back_end/model_utils.py
+++ back_end/model_utils.py
@@ -4,5 +4,4 @@ rf = joblib.load("model.pkl")
 def predict_features(features: list):
     # reminder for me, NOT CODE ↓
     # features = [type, material, region, soil_type, exact_location, temperature_c, date_of_last_repair, date_issue_was_observed, install_year, issue_description]
-    
     return str(rf.predict([features])[0])
