0c431cc32a538e4ba850a92267d75562bebe73a1
paradox1065 <kavyat1102@gmail.com>
2025-12-14 19:26:11 +0000

    Add data cleaning script for tunnel vision dataset
    - Implemented a new Python script to clean the tunnel vision dataset.
    - The cleaned data will be saved as 'tunnelvision_cleaned.csv' in the data directory.

--- back_end/model_utils.py
+++ back_end/model_utils.py
@@ -1,7 +1,8 @@
 import joblib
 
-rf = joblib.load("model.pkl")
-def predict_features(features: list):
-    # reminder for me, NOT CODE ↓
-    # features = [type, material, region, soil_type, exact_location, temperature_c, date_of_last_repair, date_issue_was_observed, install_year, issue_description]
-    return str(rf.predict([features])[0])
+failure_model = joblib.load("models/failure_30d.pkl")
+failure_type_model = joblib.load("models/failure_type.pkl")
+risk_model = joblib.load("models/risk_score.pkl")
+action_model = joblib.load("models/recommended_action.pkl")
+priority_model = joblib.load("models/priority.pkl")
+emergency_model = joblib.load("models/emergency.pkl")
