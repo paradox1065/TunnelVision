db7c0ff269f9ee49740528be527ab741592212e6
paradox1065 <kavyat1102@gmail.com>
2025-12-25 15:50:38 -0800

    Add static files and file response imports

--- back_end/api.py
+++ back_end/api.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 from pydantic import BaseModel, model_validator
 from typing import Optional, Tuple
 from datetime import date
